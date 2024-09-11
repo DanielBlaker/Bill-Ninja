6723d18925f4d71249b27a6cfc5550b5a30c3637
DanielBlaker <daniel.blaker@airwallex.com>
2024-09-11 15:01:30 +1000

    Start gyro streaming and switch to https

--- server.js
+++ server.js
@@ -1,18 +1,32 @@
 const express = require('express')
-const { createServer } = require('node:http')
+const { createServer } = require('node:https')
 const { Server } = require('socket.io');
+const fs = require('fs')
 
 const app = express()
-const server = createServer(app);
+const server = createServer({
+    key: fs.readFileSync('./key.pem'),
+    cert: fs.readFileSync('./cert.pem') 
+}, app);
 const io = new Server(server)
 
 io.on('connection', (socket) => {
     console.log('a user connected');
+
+    socket.on('acceleration', (acceleration) => {
+        console.log(acceleration)
+    })
+    socket.on('rotation', (rotation) => {
+        // console.log(JSON.stringify(rotation))
+    })
+    socket.onAny((eventName, ...args) => {
+        // console.log(eventName, args)
+    });
 });
 
 app.use(express.static('static'))
 
-server.listen(8080, function (err) {
+server.listen(443, '10.50.0.230', function (err) {
     if (err) console.log(err);
     console.log("Live at :8080")
 });
