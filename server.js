9bf64237a11acbc76a493638ac8b09bd0f732275
DanielBlaker <daniel.blaker@airwallex.com>
2024-09-11 13:33:58 +1000

    Socket IO and basic mobile page

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express')
+const { createServer } = require('node:http')
+const { Server } = require('socket.io');
+
 const app = express()
+const server = createServer(app);
+const io = new Server(server)
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
 
 app.use(express.static('static'))
 
-
-app.listen(8080, function (err) {
+server.listen(8080, function (err) {
     if (err) console.log(err);
     console.log("Live at :8080")
 });
